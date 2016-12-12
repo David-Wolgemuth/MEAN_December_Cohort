a51e67e6bc312b150eef346dba6c180ec3f861a3
Alex Ambrose <ambrose@purdue.edu>
2016-12-12 15:58:58 -0600

    updates to the server side code

--- Full_MEAN/To_Do/team_AlexA/toDoList/client/scripts/controllers/controller.js
+++ Full_MEAN/To_Do/team_AlexA/toDoList/client/scripts/controllers/controller.js
@@ -1,6 +1,9 @@
 myApp.controller('itemController', ['$scope','itemsFactory', function ($scope, itemsFactory){
     $scope.items = [];
     console.log("Something happened");
+    itemsFactory.getItems(function(data){
+        $scope.items = data;
+    })
     $scope.callAddItem = function () {
         itemsFactory.addItem($scope.newItem, function(data){
             $scope.newItem = {};
--- Full_MEAN/To_Do/team_AlexA/toDoList/server/controllers/items.js
+++ Full_MEAN/To_Do/team_AlexA/toDoList/server/controllers/items.js
@@ -1,21 +1,32 @@
 var mongoose = require('mongoose');
 var Item = mongoose.model('Item');
+
 module.exports = {
   index: function(req, res) {
       var context = {};
       Item.find({}, function(err, items) {
-          if(err) {
+        if(err) {
             console.log('something went wrong');
         }
         else {
-              console.log("SHARKS HERE", items);
+              console.log("ITEMS HERE", items);
               if(items){
                   context = {
                       items : items
                   };
               }
         };
-        res.render('index', context);
+        res.json(context);
+      });
+  },
+  create: function (req, res) {
+      var item = new Item({title: req.body.title, description: req.body.description, date: req.body.date});
+      item.save(function(err) {
+          if(err){
+              console.log("something went wrong");
+          } else {
+              res.json(item);
+          };
       });
   }
 }
