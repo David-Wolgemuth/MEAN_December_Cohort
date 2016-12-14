445eb8baba8825e60d8684cc9101c006fcae3331
EthanTang <tangrenxing@gmail.com>
2016-12-14 17:28:54 -0600

    finished the update feature on MEAN friends

--- Full_MEAN/Simple_Server/Ethan/file_structure_simple_server/client/assets/controllers/editController.js
+++ Full_MEAN/Simple_Server/Ethan/file_structure_simple_server/client/assets/controllers/editController.js
@@ -1,16 +1,12 @@
 app.controller('editController', ['$scope','friendsFactory','$routeParams', function($scope, friendsFactory,$routeParams) {
-  /*
-    GET A FRIEND FROM THE FACTORY, This is a one time thing when we load this partial -
-    so we didn't set a variable so we could reuse it -
-    we just run the friendsFactory method directly.
-  */ id = $routeParams.id
-     friendsFactory.getFriend(id,function(returnedData){
+   id = $routeParams.id;
+   friendsFactory.getFriend(id,function(returnedData){
      $scope.friend = returnedData;
    });
-  /*
-    OUR $scope.update function goes here <-- $scope because we need to access this method
-    with ng-submit or ng-click (from the form in the previous assignment).  Want to see
-    all of the friends when we get back including the updated on??
-    See Index in the previous controller.
-  */
+   $scope.updateFriend = function(friend){
+     friendsFactory.update(friend,function(returned_data){
+       index();
+       console.log("returned")
+     })
+   }
 }]);
